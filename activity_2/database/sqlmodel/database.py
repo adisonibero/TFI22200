7f7977648ee85d622c7d0580f28f105f3035b138
Adison Jimenez <adisonzenemij@gmail.com>
2026-05-23 21:09:41 -0500

    feat: aa2cf5a4-ef6f-4830-9471-ca9d086d80d9

--- activity_2/database/sqlmodel/database.py
+++
@@ -1,9 +0,0 @@
-from sqlmodel import create_engine
-
-# Con mysql-connector
-mysql_url = "mysql+mysqlconnector://root:1234@localhost:3306/mi_base"
-
-# Si usas pymysql, sería:
-# mysql_url = "mysql+pymysql://root:1234@localhost:3306/mi_base"
-
-engine = create_engine(mysql_url, echo=True)
